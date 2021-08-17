927828895513eb442603c9ecd20e5253728f06e3
Westermeister <westermeister12@gmail.com>
2021-08-17 03:31:57 -0700

    Add api entry point

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+/**
+ * Initialize backend routes.
+ * Copyright (c) 2021 Westermeister. All rights reserved.
+ */
+
+require("dotenv").config({ path: "../.env" });
+
+const express = require("express");
+
+const quotes = require("./routes/quotes");
+const register = require("./routes/register");
+
+const app = express();
+
+app.use("/quotes", quotes);
+app.use("/register", register);
+
+app.listen(process.env.PORT, "localhost");
