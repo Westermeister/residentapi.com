3221a5e768a6291e1b5c4754117fe712f699f4b2
Westermeister <westermeister12@gmail.com>
2021-08-29 12:31:56 -0700

    Refactor user auth middleware and database bindings

--- backend/database/bindings.js
+++ backend/database/bindings.js
@@ -10,10 +10,10 @@ const sqlite = require("better-sqlite3");
 
 // First, we build the static database.
 // This database holds the built database from the CSV files in the tables/ directory.
-if (fs.existsSync("../static.db")) {
-  fs.unlinkSync("../static.db");
+if (fs.existsSync("./static.db")) {
+  fs.unlinkSync("./static.db");
 }
-const staticDatabase = sqlite("../static.db");
+const staticDatabase = sqlite("./static.db");
 staticDatabase.pragma("journal_mode = WAL");
 staticDatabase
   .prepare(
@@ -30,11 +30,11 @@ for (const row of quotesTable) {
     .run(row.quote, row.author, row.context, row.source);
 }
 
-// Second, we initialize the dynamic database.
-// This is used to store user information, which is dynamically-generated added runtime.
-const dynamicDatabase = sqlite("../dynamic.db");
-dynamicDatabase.pragma("journal_mode = WAL");
-dynamicDatabase
+// Second, we initialize the user database.
+// This is used to store user information, which is dynamically added during runtime.
+const userDatabase = sqlite("./dynamic.db");
+userDatabase.pragma("journal_mode = WAL");
+userDatabase
   .prepare(
     "create table if not exists users " +
       "(id integer primary key, name text, email text, identity_key text, secret_key_hash text)"
@@ -43,5 +43,5 @@ dynamicDatabase
 
 module.exports = {
   staticDatabase,
-  dynamicDatabase,
+  userDatabase,
 };
---
+++ backend/middleware/validateUserRequest.js
@@ -0,0 +1,121 @@
+/**
+ * Middleware for validating API calls from end users.
+ * Copyright (c) 2021 Westermeister. All rights reserved.
+ */
+
+const argon2 = require("argon2");
+
+const { userDatabase } = require("../database/bindings");
+
+/**
+ * Validate incoming calls from end users to the API.
+ * @param {express.Request} req - Expects headers "identity-key" and "secret-key" i.e. the API keys.
+ * @param {express.Response} res - Used to send back error code and relevant message if sanitization failed.
+ * @param {express.NextFunction} next - Used to proceed to the next middleware if successful.
+ */
+function validateUserRequest(req, res, next) {
+  const cleanRequest = sanitizeRequest(req, res);
+  const authenticRequest = authenticateRequest(req, res);
+  if (cleanRequest && authenticRequest) {
+    next();
+  }
+}
+
+/**
+ * Ensure the incoming request is syntactically valid.
+ * @param {express.Request} req - Expects headers "identity-key" and "secret-key" i.e. the API keys.
+ * @param {express.Response} res - Used to send back error code and relevant message if sanitization failed.
+ * @returns {boolean} True if request is clean, otherwise false.
+ */
+function sanitizeRequest(req, res) {
+  // Ensure headers exist.
+  let identityHeader = req.get("identity-key");
+  let secretHeader = req.get("secret-key");
+  if (identityHeader === undefined || secretHeader === undefined) {
+    res
+      .status(400)
+      .json({ error: "Missing header(s): IDENTITY-KEY and/or SECRET-KEY" });
+    return false;
+  }
+
+  // Ensure header prefixes exist.
+  if (
+    !identityHeader.startsWith("identity-") ||
+    !secretHeader.startsWith("secret-")
+  ) {
+    res
+      .status(400)
+      .json({ error: 'API keys must have "identity-" or "secret-" prefixes!' });
+    return false;
+  }
+
+  // Ensure hex portions exist.
+  const identityHeaderSplit = identityHeader.split("-");
+  const secretHeaderSplit = secretHeader.split("-");
+  if (
+    identityHeaderSplit.length !== 2 ||
+    identityHeaderSplit[1] === "" ||
+    secretHeaderSplit.length !== 2 ||
+    secretHeaderSplit[1] === ""
+  ) {
+    res.status(400).json({
+      error:
+        "The identity and/or secret key is missing its hexadecimal portion.",
+    });
+    return false;
+  }
+
+  // Ensure hex portions are valid.
+  const keyRegex = /[0-9a-f]{64}/;
+  if (
+    !keyRegex.test(identityHeaderSplit[1]) ||
+    !keyRegex.test(secretHeaderSplit[1])
+  ) {
+    res.status(400).json({
+      error:
+        "The hexadecimal part of the identity and/or secret key is invalid. Ensure it passes regex: /[0-9a-f]{64}/",
+    });
+    return false;
+  }
+
+  // If we got through all of that, the request is clean.
+  return true;
+}
+
+/**
+ * Authenticate requests by checking the API keys against the database.
+ * @param {express.Request} req - Expects headers "identity-key" and "secret-key" i.e. the API keys.
+ * @param {express.Response} res - Used to send back error code and relevant message if authentication failed.
+ * @returns {boolean} True if request is authentic, otherwise false.
+ */
+async function authenticateRequest(req, res) {
+  // First, verify that the user exists by checking for the identity key,
+  const userExists = userDatabase
+    .prepare("select count(1) from users where identity_key = ? limit 1")
+    .get(req.get("identity-key"))["count(1)"];
+  if (!userExists) {
+    res.status(401).json({ error: "Identity key is not recognized." });
+    return false;
+  }
+
+  // Second, ensure that the secret key matches.
+  const secretKeyHash = userDatabase
+    .prepare("select secret_key_hash from users where identity_key = ? limit 1")
+    .get(req.get("identity-key")).secret_key_hash;
+  try {
+    if (await argon2.verify(secretKeyHash, req.get("secret-key"))) {
+      return true;
+    } else {
+      res.status(401).json({ error: "Secret key is invalid." });
+      return false;
+    }
+  } catch {
+    res.status(500).json({
+      error:
+        "Server encountered unknown error while trying to verify secret key. Please try again later.",
+    });
+    return false;
+  }
+}
+
+module.exports = validateUserRequest;
