cfa161a7cff261853e5301d0b1eaf902b4338e8d
Westermeister <westermeister12@gmail.com>
2021-08-30 16:31:38 -0700

    Refactor user registration route handler

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -3,106 +3,111 @@
  * Copyright (c) 2021 Westermeister. All rights reserved.
  */
 
-require("dotenv").config({ path: "../../.env" });
-
 const crypto = require("crypto");
 
 const argon2 = require("argon2");
 const express = require("express");
-const fetch = require("node-fetch");
 
-const { dynamicDatabase } = require("../utils/database");
+const { userDatabase } = require("../database/bindings");
 
-const router = express.Router();
-router.use(express.urlencoded({ extended: true }));
+/**
+ * Filters out requests that have filled the honeypot field.
+ * @param {express.Request} req - Should have an "address" body param that actually serves as the honeypot.
+ * @returns {boolean} True if request is spam, false otherwise.
+ */
+function isSpam(req) {
+  return typeof req.body.address !== "string" || req.body.address.length > 0;
+}
 
-router.post("/", async (req, res) => {
-  // Check for required parameters and their types.
-  if (
-    req.body.captcha === undefined ||
-    req.body.name === undefined ||
-    req.body.email === undefined ||
-    typeof req.body.captcha !== "string" ||
-    typeof req.body.name !== "string" ||
-    typeof req.body.email !== "string"
-  ) {
-    res.status(400).send("Malformed request");
-    return;
-  }
-
-  // Verify that the captcha was successful.
-  const body = new URLSearchParams();
-  body.append("sitekey", process.env.H_CAPTCHA_SITEKEY);
-  body.append("secret", process.env.H_CAPTCHA_SECRET);
-  body.append("response", req.body.captcha);
-  body.append("remoteip", req.ip);
-  try {
-    const fetchedCaptchaResponse = await fetch(
-      "https://hcaptcha.com/siteverify",
-      {
-        method: "post",
-        body: body,
-      }
-    );
-    const captchaResponse = await fetchedCaptchaResponse.json();
-    if (!captchaResponse.success) {
-      res.status(401).send("Failed to verify captcha");
-      return;
-    }
-  } catch {
-    res.status(500).send("Failed to verify captcha");
-    return;
-  }
-
-  // Prepare to add user to database.
-  // Start by capping max length of name and email values in case of spam.
-  req.body.name = req.body.name.substr(0, 1000);
-  req.body.email = req.body.email.substr(0, 1000);
-
-  // While we're at it, let's make sure that we don't have a duplicate email.
-  const checkDuplicateEmail = dynamicDatabase.prepare(
-    "select count(1) from users where email = ? limit 1"
+/**
+ * Verify presence of required inputs and their types.
+ * @param {express.Request} req - Should have "name" and "email" body params.
+ * @returns {boolean} True if valid, false otherwise.
+ */
+function validInputs(req) {
+  return (
+    typeof req.body.name === "string" && typeof req.body.email === "string"
   );
-  const duplicateEmail = checkDuplicateEmail.get(req.body.email)["count(1)"];
+}
+
+/**
+ * Generates unique identity key.
+ * @returns {string} "identity-" + a unique, 64-char hex string.
+ */
+function generateIdentityKey() {
+  let identityKey = "identity-" + crypto.randomBytes(32).toString("hex");
+  let numDuplicates = userDatabase
+    .prepare("select count(1) from users where identity_key = ? limit 1")
+    .get(identityKey)["count(1)"];
+  while (numDuplicates > 0) {
+    identityKey = "identity-" + crypto.randomBytes(32).toString("hex");
+    numDuplicates = userDatabase
+      .prepare("select count(1) from users where identity_key = ? limit 1")
+      .get(identityKey)["count(1)"];
+  }
+  return identityKey;
+}
+
+/**
+ * Generates secret key and a salted hash.
+ * @returns {list} First the raw secret key, then its salted hash via Argon2id.
+ */
+async function generateSecretKeyPair() {
+  const secretKey = "secret-" + crypto.randomBytes(32).toString("hex");
+  const secretKeyHash = await argon2.hash(secretKey, { type: argon2.argon2id });
+  return [secretKey, secretKeyHash];
+}
+
+/**
+ * Register a new user.
+ * @param {express.Request} req - Should have "name", "email", and "address" (honeypot) body parameters
+ * @param {express.Response} res - Used to send back status codes and/or API keys.
+ */
+async function registerUser(req, res) {
+  // Send a fake success response if we detect spam.
+  if (isSpam(req)) {
+    res.status(200).json({ message: "Sign up successful" });
+    return;
+  }
+
+  // Verify that the rest of the inputs are syntactically valid.
+  if (!validInputs(req)) {
+    res.status(400).json({ error: 'Invalid "name" and/or "email" inputs.' });
+    return;
+  }
+
+  // Before we go further, cap name and email strings to prevent abuse.
+  const name = req.body.name.substr(0, 1000);
+  const email = req.body.email.substr(0, 1000);
+
+  // Ensure email isn't a duplicate.
+  const duplicateEmail = userDatabase
+    .prepare("select count(1) from users where email = ? limit 1")
+    .get(email)["count(1)"];
   if (duplicateEmail) {
     res
       .status(409)
-      .send(`User already exists with given email: ${req.body.email}`);
+      .json({ error: `User already exists with given email: ${email}` });
     return;
   }
 
-  // Generate identity key. Make sure it doesn't already exist.
-  let identityKey = "identity-" + crypto.randomBytes(32).toString("hex");
-  const getDuplicates = dynamicDatabase.prepare(
-    "select count(1) from users where identity_key = ? limit 1"
-  );
-  let duplicates = getDuplicates.get(identityKey)["count(1)"];
-  while (duplicates > 0) {
-    identityKey = "identity-" + crypto.randomBytes(32).toString("hex");
-    duplicates = getDuplicates.get(identityKey)["count(1)"];
-  }
-
-  // Generate secret key and the Argon2id hash.
-  const secretKey = "secret-" + crypto.randomBytes(32).toString("hex");
-  // Using recommended parameters from Argon2's IETF draft; version 13, option 2:
-  // https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-argon2-13#page-13
-  const secretKeyHash = await argon2.hash(secretKey, {
-    type: argon2.argon2id,
-    timeCost: 3,
-    parallelism: 4,
-    memoryCost: 2 ** 16,
-    saltLength: 16,
-    hashLength: 32,
-  });
+  // We're all good at this point. Let's generate the API keys.
+  const identityKey = generateIdentityKey();
+  const [secretKey, secretKeyHash] = await generateSecretKeyPair();
 
   // Add user to database.
-  const addUser = dynamicDatabase.prepare(
-    "insert into users (name, email, identity_key, secret_key_hash) values (?, ?, ?, ?)"
-  );
-  addUser.run(req.body.name, req.body.email, identityKey, secretKeyHash);
+  userDatabase
+    .prepare(
+      "insert into users (name, email, identity_key, secret_key_hash) values (?, ?, ?, ?)"
+    )
+    .run(name, email, identityKey, secretKeyHash);
 
-  // Return the identity and secret keys to the frontend.
+  // Finally, respond with the API keys.
   res.status(201).json({ identityKey: identityKey, secretKey: secretKey });
-});
+}
+
+const router = express.Router();
+router.use(express.urlencoded({ extended: true }));
+router.post("/", registerUser);
 
 module.exports = router;
