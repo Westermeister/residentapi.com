bc1e7a5706bafb7cee3f7526d336186da8dc65cd
Westermeister <westermeister12@gmail.com>
2021-09-01 12:18:39 -0700

    Refactor user registration route handler

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -11,23 +11,66 @@ const express = require("express");
 const { userDatabase } = require("../database/bindings");
 
 /**
- * Filters out requests that have filled the honeypot field.
- * @param {express.Request} req - Should have an "address" body param that actually serves as the honeypot.
- * @returns {boolean} True if request is spam, false otherwise.
+ * Verify presence of required inputs and their required datatypes.
+ * @param {express.Request} req - Should have "name" and "email" body params.
+ * @param {express.Response} res - Used to send an error message if the inputs aren't valid.
+ * @returns {boolean} True if valid, false otherwise.
  */
-function isSpam(req) {
-  return typeof req.body.address !== "string" || req.body.address.length > 0;
+function validateInputs(req, res) {
+  if (
+    typeof req.body.name !== "string" ||
+    typeof req.body.reason !== "string" ||
+    typeof req.body.email !== "string" ||
+    req.body.name.length === 0 ||
+    req.body.email.length === 0
+  ) {
+    res
+      .status(400)
+      .json({ error: 'Invalid "name", "reason", or "email" inputs.' });
+    return false;
+  }
+  return true;
 }
 
 /**
- * Verify presence of required inputs and their types.
- * @param {express.Request} req - Should have "name" and "email" body params.
- * @returns {boolean} True if valid, false otherwise.
+ * Filters out requests that have filled the honeypot field.
+ * @param {express.Request} req - Should have an "reason" body param that actually serves as the honeypot.
+ * @param {express.Response} res - Used to send a fake success response in case of spam.
+ * @returns {boolean} True if request is spam, false otherwise.
  */
-function validInputs(req) {
-  return (
-    typeof req.body.name === "string" && typeof req.body.email === "string"
-  );
+function detectSpam(req, res) {
+  if (req.body.reason.length > 0) {
+    res.status(200).json({ message: "Sign up successful" });
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Cap the maximum length of the inputs to prevent abuse.
+ * @param {express.Request} req - Should have "name" and "email" params in the POST body.
+ */
+function capInputs(req) {
+  return [req.body.name.substr(0, 1000), req.body.email.substr(0, 1000)];
+}
+
+/**
+ * Ensure that the user's email isn't already in the database.
+ * @param {string} email - The user's email address.
+ * @param {express.Response} res - Used to send an error response in case of a duplicate.
+ * @returns {boolean} True if email is unique, false otherwise.
+ */
+function verifyUniqueEmail(email, res) {
+  const duplicateEmail = userDatabase
+    .prepare("select count(1) from users where email = ? limit 1")
+    .get(email)["count(1)"];
+  if (duplicateEmail) {
+    res
+      .status(409)
+      .json({ error: `User already exists with given email: ${email}` });
+    return false;
+  }
+  return true;
 }
 
 /**
@@ -60,54 +103,31 @@ async function generateSecretKeyPair() {
 
 /**
  * Register a new user.
- * @param {express.Request} req - Should have "name", "email", and "address" (honeypot) body parameters
- * @param {express.Response} res - Used to send back status codes and/or API keys.
+ * @param {express.Request} req - Should have "name", "email", and "reason" (honeypot) params in POST body.
+ * @param {express.Response} res - Used to send back status codes, errors, and/or API keys.
  */
 async function registerUser(req, res) {
-  // Send a fake success response if we detect spam.
-  if (isSpam(req)) {
-    res.status(200).json({ message: "Sign up successful" });
-    return;
+  const validInputs = validateInputs(req, res);
+  const isSpam = detectSpam(req, res);
+  if (validInputs && !isSpam) {
+    const [name, email] = capInputs(req);
+    const uniqueEmail = verifyUniqueEmail(email, res);
+    // If true, then we're all good from here on out to add the user like normal.
+    if (uniqueEmail) {
+      const identityKey = generateIdentityKey();
+      const [secretKey, secretKeyHash] = await generateSecretKeyPair();
+      userDatabase
+        .prepare(
+          "insert into users (name, email, identity_key, secret_key_hash) values (?, ?, ?, ?)"
+        )
+        .run(name, email, identityKey, secretKeyHash);
+      res.status(201).json({ identityKey: identityKey, secretKey: secretKey });
+    }
   }
-
-  // Verify that the rest of the inputs are syntactically valid.
-  if (!validInputs(req)) {
-    res.status(400).json({ error: 'Invalid "name" and/or "email" inputs.' });
-    return;
-  }
-
-  // Before we go further, cap name and email strings to prevent abuse.
-  const name = req.body.name.substr(0, 1000);
-  const email = req.body.email.substr(0, 1000);
-
-  // Ensure email isn't a duplicate.
-  const duplicateEmail = userDatabase
-    .prepare("select count(1) from users where email = ? limit 1")
-    .get(email)["count(1)"];
-  if (duplicateEmail) {
-    res
-      .status(409)
-      .json({ error: `User already exists with given email: ${email}` });
-    return;
-  }
-
-  // We're all good at this point. Let's generate the API keys.
-  const identityKey = generateIdentityKey();
-  const [secretKey, secretKeyHash] = await generateSecretKeyPair();
-
-  // Add user to database.
-  userDatabase
-    .prepare(
-      "insert into users (name, email, identity_key, secret_key_hash) values (?, ?, ?, ?)"
-    )
-    .run(name, email, identityKey, secretKeyHash);
-
-  // Finally, respond with the API keys.
-  res.status(201).json({ identityKey: identityKey, secretKey: secretKey });
 }
 
 const router = express.Router();
-router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
 router.post("/", registerUser);
 
 module.exports = router;
